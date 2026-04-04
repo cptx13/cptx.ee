9291088971fddaa45c2b520579bed2459b24d37c
cptx <cpinedotexidor@proton.me>
2026-04-04 11:30:33 +0000

    Style: Shuffle button

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -1,10 +1,10 @@
 <template>
 <div class="header">
-  <div style="display: flex; align-items: center; gap: 0.5em; width: 100%;">
+  <div style="display: flex; align-items: stretch; gap: 0.5em; width: 100%;">
     <h1 class="header-title">
       <a href="/">cptx</a>
     </h1>
-    <a id="random-post-button" title="Shuffle post" aria-label="Shuffle post" href="#" style="display: inline-flex; align-items: center; justify-content: center; position: relative; top: 8px;">
+    <a id="random-post-button" title="Shuffle post" aria-label="Shuffle post" href="#" class="shuffle-button">
       <svg role="img" fill="currentColor" viewBox="0 0 24 24" style="height: 28px; width: 28px;">
         <title>Shuffle</title>
         <path d="M16 3h5v5h-2V6.41l-3.29 3.3-1.42-1.42L17.59 5H16V3Zm3 14h2v4h-5v-2h1.59l-3.3-3.29 1.42-1.42L19 17.59V17ZM3 7h3.59l8 8H21v2h-7.41l-8-8H3V7Zm0 10v-2h2.59l2-2 1.42 1.42L6.41 17H3Z"/>
