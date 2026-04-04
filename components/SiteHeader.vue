dfe4554b166488d3852ee56a80ab9ad42105353f
cptx <cpinedotexidor@proton.me>
2026-04-04 10:58:24 +0000

    style: reorder navigation menu and changed font

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -13,12 +13,11 @@
   </div>
   <div class="header-menu">
     <p class="small"><a href="/">/home</a></p>
+    <p class="small"><a href="/manifesto/">/manifesto</a></p>
+    <p class="small"><a href="/categories/pockets/">/pockets</a></p>
     <p class="small"><a href="/categories/croquis/">/croquis</a></p>
-    <p class="small"><a href="/categories/manuscripts/">/manuscripts</a></p>
     <p class="small"><a href="/categories/recuerdos/">/recuerdos</a></p>
     <p class="small"><a href="/categories/archives/">/archives</a></p>
-    <p class="small"><a href="/categories/pockets/">/pockets</a></p>
-    <p class="small"><a href="/manifesto/">/manifesto</a></p>
   </div>
 </div>
 </template>
