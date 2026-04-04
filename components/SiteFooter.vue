70531d6f186fa9746fe2e5612b2a6fe6a56acde8
Carlos Pinedo Texidor <carlos@cptx.ee>
2026-04-04 12:33:40 +0000

    fix: footer copyright to cptx with LinkedIn link
    Co-authored-by: Shelley <shelley@exe.dev>

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -1,7 +1,7 @@
 <template>
 <footer>
   <div style="color: #888; font-size: 0.95em; text-align: center; margin-bottom: 2em;">
-    Powered by <a href="https://www.github.com/dhamidi/htmlc">htmlc</a> - &copy; 2026 Carlos Pinedo Texidor
+    Powered by <a href="https://www.github.com/dhamidi/htmlc">htmlc</a> - &copy; 2026 <a href="https://www.linkedin.com/in/cptx/">cptx</a>
   </div>
 </footer>
 </template>
