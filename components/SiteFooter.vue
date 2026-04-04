48b6403c3210351fdffe00abe8dd2511ba4b2f3c
exe.dev user <exedev@cptxee.exe.xyz>
2026-04-04 12:08:03 +0000

    fix: htmlc footer link to use full URL
    Co-authored-by: Shelley <shelley@exe.dev>

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -1,7 +1,7 @@
 <template>
 <footer>
   <div style="color: #888; font-size: 0.95em; text-align: center; margin-bottom: 2em;">
-    Powered by <a href="github.com/dhamidi/htmlc">htmlc</a> - &copy; 2026 Carlos Pinedo Texidor
+    Powered by <a href="https://www.github.com/dhamidi/htmlc">htmlc</a> - &copy; 2026 Carlos Pinedo Texidor
   </div>
 </footer>
 </template>
