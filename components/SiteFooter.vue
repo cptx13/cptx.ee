086f2cd1955cc0e33a4a2ffc5cd2d1ba317ffd01
cptx <cpinedotexidor@proton.me>
2026-04-04 09:27:03 +0000

    Style SiteFooter.vue      * Remove Typo reference      * Remove Hugo reference      * Add htmlc reference

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -1,7 +1,7 @@
 <template>
 <footer>
   <div style="color: #888; font-size: 0.95em; text-align: center; margin-bottom: 2em;">
-    Powered by <a href="https://gohugo.io/">Hugo</a> and <a href="https://github.com/tomfran/typo">Typo</a> &copy; 2026 Carlos Pinedo Texidor
+    Powered by <a href="github.com/dhamidi/htmlc">htmlc</a> - &copy; 2026 Carlos Pinedo Texidor
   </div>
 </footer>
 </template>
